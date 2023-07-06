6e47f1bf7860e118f23eecb7dbf39761267db4b8
countercrow <hongoppa@naver.com>
2023-07-06 12:13:48 +0900

    멤버 DTO , VO정리

---
+++ src/main/java/com/crowmarket/app/infra/commen/member/member.java
@@ -0,0 +1,60 @@
+package com.crowmarket.app.infra.commen.member;
+
+import com.crowmarket.app.infra.commen.base.base;
+
+public class member extends base{
+
+private String memberSeq;
+private String memberNY;
+private String memberId;
+private String memberPassword;
+private String memberName;
+private String memberEmail;
+private String memberPhone;
+
+/*--------------------------------------------------------*/
+public String getMemberSeq() {
+	return memberSeq;
+}
+public void setMemberSeq(String memberSeq) {
+	this.memberSeq = memberSeq;
+}
+public String getMemberNY() {
+	return memberNY;
+}
+public void setMemberNY(String memberNY) {
+	this.memberNY = memberNY;
+}
+public String getMemberId() {
+	return memberId;
+}
+public void setMemberId(String memberId) {
+	this.memberId = memberId;
+}
+public String getMemberPassword() {
+	return memberPassword;
+}
+public void setMemberPassword(String memberPassword) {
+	this.memberPassword = memberPassword;
+}
+public String getMemberName() {
+	return memberName;
+}
+public void setMemberName(String memberName) {
+	this.memberName = memberName;
+}
+public String getMemberEmail() {
+	return memberEmail;
+}
+public void setMemberEmail(String memberEmail) {
+	this.memberEmail = memberEmail;
+}
+public String getMemberPhone() {
+	return memberPhone;
+}
+public void setMemberPhone(String memberPhone) {
+	this.memberPhone = memberPhone;
+}
+
+
+}
---
+++ src/main/java/com/crowmarket/app/infra/commen/member/memberVo.java
@@ -0,0 +1,88 @@
+package com.crowmarket.app.infra.commen.member;
+
+import com.crowmarket.app.infra.commen.base.PageVo;
+
+public class memberVo extends PageVo{
+	
+	private String memberSeq;
+	private String keySeq;
+	private String keyName;
+	private String keyID;
+	private String keyGender;
+	private String keyEmail;
+	private String keyLocation;
+	private String keyPostNUM;
+	private String keyRegDT;
+	private String keyModDT;
+	private String keyRemDT;
+	
+	/*----------------------------------------------*/
+	public String getMemberSeq() {
+		return memberSeq;
+	}
+	public void setMemberSeq(String memberSeq) {
+		this.memberSeq = memberSeq;
+	}
+	public String getKeySeq() {
+		return keySeq;
+	}
+	public void setKeySeq(String keySeq) {
+		this.keySeq = keySeq;
+	}
+	public String getKeyName() {
+		return keyName;
+	}
+	public void setKeyName(String keyName) {
+		this.keyName = keyName;
+	}
+	public String getKeyID() {
+		return keyID;
+	}
+	public void setKeyID(String keyID) {
+		this.keyID = keyID;
+	}
+	public String getKeyGender() {
+		return keyGender;
+	}
+	public void setKeyGender(String keyGender) {
+		this.keyGender = keyGender;
+	}
+	public String getKeyEmail() {
+		return keyEmail;
+	}
+	public void setKeyEmail(String keyEmail) {
+		this.keyEmail = keyEmail;
+	}
+	public String getKeyLocation() {
+		return keyLocation;
+	}
+	public void setKeyLocation(String keyLocation) {
+		this.keyLocation = keyLocation;
+	}
+	public String getKeyPostNUM() {
+		return keyPostNUM;
+	}
+	public void setKeyPostNUM(String keyPostNUM) {
+		this.keyPostNUM = keyPostNUM;
+	}
+	public String getKeyRegDT() {
+		return keyRegDT;
+	}
+	public void setKeyRegDT(String keyRegDT) {
+		this.keyRegDT = keyRegDT;
+	}
+	public String getKeyModDT() {
+		return keyModDT;
+	}
+	public void setKeyModDT(String keyModDT) {
+		this.keyModDT = keyModDT;
+	}
+	public String getKeyRemDT() {
+		return keyRemDT;
+	}
+	public void setKeyRemDT(String keyRemDT) {
+		this.keyRemDT = keyRemDT;
+	}
+	
+
+}
