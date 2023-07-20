0074db7231621eca337f79b5765083344595b817
countercrow <hongoppa@naver.com>
2023-07-20 16:53:31 +0900

    유저 인터셉터 변경(인터셉터에서 컨트롤러역활까지 부여했었음)

--- src/main/java/com/crowmarket/app/commen/interceptor/CheckLoginSessionInterceptionUser.java
+++ src/main/java/com/crowmarket/app/commen/interceptor/CheckLoginSessionInterceptionUser.java
@@ -2,7 +2,6 @@ package com.crowmarket.app.commen.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -10,34 +9,19 @@ public class CheckLoginSessionInterceptionUser extends HandlerInterceptorAdapter
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-	        throws Exception {
-
-	    HttpSession session = request.getSession();
-	    String sessionAdminNYStr = (String) session.getAttribute("sessionAdminNY");
-	    Integer sessionAdminNY = null;
-
-	    if (sessionAdminNYStr != null) {
-	        try {
-	            sessionAdminNY = Integer.parseInt(sessionAdminNYStr);
-	        } catch (NumberFormatException e) {
-	            // 캐스팅 실패 시 처리
-	            e.printStackTrace();
-	        }
-	    }
-
-	    if (session.getAttribute("sessionId") != null && sessionAdminNY != null) {
-	        if (sessionAdminNY != 0) {
-	        	response.sendRedirect("/userLogin");
-	        	System.out.println("관리자");
-	        } else {
-	            System.out.println("일반 회원");
-	        }
-	    } else {
-	        response.sendRedirect("/userLogin");
-	        System.out.println("Redirect to login");
-	        return true;
-	    }
-
-	    return super.preHandle(request, response, handler);
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+		if (request.getSession().getAttribute("sessionUserId") != null) {
+			// by pass
+			System.out.println("유저 인터셉터");
+		} else {
+			response.sendRedirect("/userLogin");
+			System.out.println("유저 인터셉터 실패");
+            return false;
+		}
+		
+		return super.preHandle(request, response, handler);
 	}
+	  
 }
