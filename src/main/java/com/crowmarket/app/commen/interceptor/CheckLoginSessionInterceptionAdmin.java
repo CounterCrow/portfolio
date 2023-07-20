a3ba37537d10bc3fe433ff72625a81d418fc8dcb
countercrow <hongoppa@naver.com>
2023-07-20 16:52:47 +0900

    안쓰는 임포트 제거

--- src/main/java/com/crowmarket/app/commen/interceptor/CheckLoginSessionInterceptionAdmin.java
+++ src/main/java/com/crowmarket/app/commen/interceptor/CheckLoginSessionInterceptionAdmin.java
@@ -2,7 +2,6 @@ package com.crowmarket.app.commen.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -10,34 +9,20 @@ public class CheckLoginSessionInterceptionAdmin extends HandlerInterceptorAdapte
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-	        throws Exception {
-
-	    HttpSession session = request.getSession();
-	    String sessionAdminNYStr = (String) session.getAttribute("sessionAdminNY");
-	    Integer sessionAdminNY = null;
-
-	    if (sessionAdminNYStr != null) {
-	        try {
-	            sessionAdminNY = Integer.parseInt(sessionAdminNYStr);
-	        } catch (NumberFormatException e) {
-	            // 캐스팅 실패 시 처리
-	            e.printStackTrace();
-	        }
-	    }
-
-	    if (session.getAttribute("sessionId") != null && sessionAdminNY != null) {
-	        if (sessionAdminNY != 1) {
-	        	response.sendRedirect("/portfolioLoginAdmin");
-	        	System.out.println("일반 회원");
-	        } else {
-	            System.out.println("관리자");
-	        }
-	    } else {
-	        response.sendRedirect("/portfolioLoginAdmin");
-	        System.out.println("Redirect to login2");
-	        return false;
-	    }
-
-	    return super.preHandle(request, response, handler);
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+		if (request.getSession().getAttribute("sessionAdminId") != null) {
+			// by pass
+			System.out.println("어드민 인터셉터");
+			
+		} else {
+			response.sendRedirect("/portfolioLoginAdmin");
+			System.out.println("어드민 인터셉터 실패"+request.getSession().getAttribute("sessionAdminId"));
+            return false;
+		}
+		
+		return super.preHandle(request, response, handler);
 	}
+
 }
