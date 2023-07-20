38dda833683089fb87a39dcbd01a41c2f3fdfeb2
countercrow <hongoppa@naver.com>
2023-07-20 16:52:34 +0900

    안쓰는 임포트 제거

--- src/main/java/com/crowmarket/app/bios/home/homeController.java
+++ src/main/java/com/crowmarket/app/bios/home/homeController.java
@@ -1,11 +1,7 @@
 package com.crowmarket.app.bios.home;
 
-	import java.io.IOException;
-	import javax.servlet.http.HttpServletResponse;
 	import javax.servlet.http.HttpSession;
-
 	import org.springframework.stereotype.Controller;
-	import org.springframework.web.bind.annotation.GetMapping;
 	import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -43,19 +39,20 @@ public class homeController {
 		  
 		  return "projact01/infra/index/index"; }
 	  
-	  @GetMapping("/img/icon/heart.png")
-	  public void getHeartIcon(HttpServletResponse response) throws IOException {
-	      // 이미지 파일을 응답으로 전송합니다.
-	      // 이미지 파일의 경로 및 전송 방식에 따라 코드를 작성해야 합니다.
-	  }
 	  @RequestMapping(value="/signUp") public String signUp() {
 		  
 		  return "projact01/infra/member/signUp"; }
+	  
 	  @RequestMapping(value="/userLogout") public String logOut(HttpSession httpSession) {
 		  httpSession.invalidate();
 		  return "redirect:/projact01"; }
+	  
 	  @RequestMapping(value="/shop") public String shop() {
 		  
 		  return "projact01/infra/subpage/shop"; }
+	  	
+	  @RequestMapping(value="/shopDetails") public String shopDetails() {
+		  
+		  return "projact01/infra/subpage/shopDetails"; }
 	 
 }
