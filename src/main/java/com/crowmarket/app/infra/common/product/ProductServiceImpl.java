1125c578bd8a41caa035528be15a9a8c2312822a
countercrow <hongoppa@naver.com>
2023-07-27 14:43:58 +0900

    생산품 캐쉬 관련 서비스 추가

--- src/main/java/com/crowmarket/app/infra/common/product/ProductServiceImpl.java
+++ src/main/java/com/crowmarket/app/infra/common/product/ProductServiceImpl.java
@@ -1,10 +1,14 @@
 package com.crowmarket.app.infra.common.product;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+
 @Service
 public class ProductServiceImpl implements ProductService{
 
@@ -51,6 +55,29 @@ public class ProductServiceImpl implements ProductService{
 	public int save(Product dto) {
 		// TODO Auto-generated method stub
 		return dao.save(dto);
+	}
+	@PostConstruct
+	public void selectlistCashedProductArrayList() throws Exception{
+		List<Product> ProductListFormDb = (ArrayList<Product>) dao.selectlistCashedProductArrayList();
+		ProductListFormDb = (ArrayList<Product>) dao.selectlistCashedProductArrayList();
+		Product.cashedProductArrayList.clear();
+		Product.cashedProductArrayList.addAll(ProductListFormDb);
+		System.out.println("CashedProductArrayList: "+Product.cashedProductArrayList.size()+"cashed!");
+	}
+	public static void clear() throws Exception{
+		Product.cashedProductArrayList.clear();
+	}
+	
+	public static List<Product> selectListCachedProduct() throws Exception{
+		List<Product> rt = new ArrayList<Product>();
+		for(Product ProductRow : Product.cashedProductArrayList) {
+			if(ProductRow.getDelNY() == 0) {
+			rt.add(ProductRow);
+		}else {
+			//by pass
+		}
+	}
+		return rt;
 	}
 
 	
