fbb1dd5b360c6ed97d1cf755b1ee4dd75da55658
countercrow <hongoppa@naver.com>
2023-07-28 12:51:04 +0900

    상수 파일에 년월일 데이터 타입선언

--- src/main/java/com/crowmarket/app/common/constants/Constants.java
+++ src/main/java/com/crowmarket/app/common/constants/Constants.java
@@ -2,6 +2,14 @@ package com.crowmarket.app.common.constants;
 
 public final class Constants {
 
+	
+	
+	
+	
+	
+	public static final String DATETIME_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
+	
+	
 	public static final String UPLOAD_PATH_PREFIX_EXTERNAL = "D://hongyounggi_Don't_Remove/factory/ws_sts4_4181/portfolio/src/main/webapp";
 	public static final String UPLOAD_PATH_PREFIX = "D://hongyounggi_Don't_Remove/factory/ws_sts4_4181/portfolio/src/main/webapp/resources/projact1/uploaded";
 	public static final String UPLOAD_PATH_PREFIX_FOR_VIEW = "/resources/projact1/uploaded";
