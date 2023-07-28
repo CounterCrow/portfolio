4bbd43cfe0806b4f2d896c0ec0c737dd052487af
countercrow <hongoppa@naver.com>
2023-07-28 12:54:09 +0900

    유틸 임포트

--- src/main/java/com/crowmarket/app/infra/common/product/ProductServiceImpl.java
+++ src/main/java/com/crowmarket/app/infra/common/product/ProductServiceImpl.java
@@ -1,13 +1,19 @@
 package com.crowmarket.app.infra.common.product;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.crowmarket.app.common.constants.Constants;
+import com.crowmarket.app.common.util.UtilDateTime;
 
 @Service
 public class ProductServiceImpl implements ProductService{
@@ -80,5 +86,48 @@ public class ProductServiceImpl implements ProductService{
 		return rt;
 	}
 
+public void uploadFiles(MultipartFile[] multipartFiles, Product dto, String tableName, int type, int maxNumber) throws Exception {
+		
+		for(int i=0; i<multipartFiles.length; i++) {
+    	
+			if(!multipartFiles[i].isEmpty()) {
+				
+				String className = dto.getClass().getSimpleName().toString().toLowerCase();		
+				String fileName = multipartFiles[i].getOriginalFilename();
+				String ext = fileName.substring(fileName.lastIndexOf(".") + 1);
+				String uuid = UUID.randomUUID().toString();
+				String uuidFileName = uuid + "." + ext;
+				String pathModule = className;
+				String nowString = UtilDateTime.nowString();
+				String pathDate = nowString.substring(0,4) + "/" + nowString.substring(5,7) + "/" + nowString.substring(8,10); 
+				String path = Constants.UPLOAD_PATH_PREFIX + "/" + pathModule + "/" + pathDate + "/";
+				String pathForView = Constants.UPLOAD_PATH_PREFIX_FOR_VIEW + "/" + pathModule + "/" + pathDate + "/";
+				
+				File uploadPath = new File(path);
+				
+				if (!uploadPath.exists()) {
+					uploadPath.mkdir();
+				} else {
+					// by pass
+				}
+				  
+				multipartFiles[i].transferTo(new File(path + uuidFileName));
+				
+				dto.setPath(pathForView);
+				dto.setOriginalName(fileName);
+				dto.setUuidName(uuidFileName);
+				dto.setExt(ext);
+				dto.setSize(multipartFiles[i].getSize());
+				
+				dto.setTableName(tableName);
+				dto.setType(type);
+//				dto.setDefaultNy();
+				dto.setSort(maxNumber + i);
+				dto.setPseq(dto.getProductSeq());
+
+				dao.insertUploaded(dto);
+    		}
+		}
+	}
 	
 }
