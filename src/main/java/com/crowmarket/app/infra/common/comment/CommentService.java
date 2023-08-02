f59cc721577ad4a2b524dacb089c42347927f84c
countercrow <hongoppa@naver.com>
2023-08-02 17:42:06 +0900

    코멘트 서비스

---
+++ src/main/java/com/crowmarket/app/infra/common/comment/CommentService.java
@@ -0,0 +1,14 @@
+package com.crowmarket.app.infra.common.comment;
+
+import java.util.List;
+
+public interface CommentService {
+
+	public List<Comment> selectListComment(CommentVo vo);
+	public List<Comment> selectListReComment(CommentVo vo);
+	public int selectOneCount(CommentVo vo);
+	public int update(Comment dto);
+	public int updele(Comment dto);
+	public int saveComment(Comment dto);
+	public int saveReComment(Comment dto);
+}
