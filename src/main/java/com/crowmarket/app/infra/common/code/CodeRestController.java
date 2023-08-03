cb9bf52942bba2a257cebbc7b46b9fb60b84b178
countercrow <hongoppa@naver.com>
2023-08-03 16:36:47 +0900

    RestAPI를 사용하기 위한 Rest컨트롤러 생성{데이터 뿌려주기}

---
+++ src/main/java/com/crowmarket/app/infra/common/code/CodeRestController.java
@@ -0,0 +1,49 @@
+package com.crowmarket.app.infra.common.code;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value="/rest/code")
+public class CodeRestController {
+	
+	@Autowired CodeServiceImpl service;
+	
+	@GetMapping("")
+	public List<Code> selectList(CodeVo vo)throws Exception{
+		List<Code> shlist = service.shSelectList(vo);
+		
+		
+		return shlist;
+	}
+	
+	@GetMapping("/{seq}")
+	public Code selectOne(@PathVariable String seq,CodeVo vo)throws Exception{
+		vo.setCodeSeq(seq);
+		Code item = service.selectOne(vo);
+		return item;
+	}
+	@RequestMapping(value = "/{seq}", method = RequestMethod.PATCH)
+//	@PatchMapping("/{seq}")
+	public void update(@PathVariable String seq, @RequestBody Code dto) throws Exception {
+		dto.setCodeSeq(seq);
+		service.update(dto);
+	}
+	
+//	@RequestMapping(value = "/{seq}", method = RequestMethod.PATCH)
+	@DeleteMapping("/{seq}")
+	public void delete(@PathVariable String seq, @RequestBody Code dto) throws Exception {
+		dto.setCodeSeq(seq);
+		service.delete(dto);
+	}
+
+}
