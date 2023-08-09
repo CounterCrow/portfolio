c7772b373a404cd2374b590ad1ad507174e579ee
countercrow <hongoppa@naver.com>
2023-08-09 17:34:39 +0900

    비교 테이블을 저장할 DTO생성중....

---
+++ src/main/java/com/crowmarket/app/infra/modules/competition/Competition.java
@@ -0,0 +1,42 @@
+package com.crowmarket.app.infra.modules.competition;
+
+import com.crowmarket.app.infra.common.base.Base;
+
+public class Competition extends Base{
+	
+	private String cometitionSeq;
+	private String product_productSeq;
+	private String product_productSeq1;
+	private String member_memberSeq;
+	
+	/*---------------------------------*/
+	
+	public String getCometitionSeq() {
+		return cometitionSeq;
+	}
+	public void setCometitionSeq(String cometitionSeq) {
+		this.cometitionSeq = cometitionSeq;
+	}
+	public String getProduct_productSeq() {
+		return product_productSeq;
+	}
+	public void setProduct_productSeq(String product_productSeq) {
+		this.product_productSeq = product_productSeq;
+	}
+	public String getProduct_productSeq1() {
+		return product_productSeq1;
+	}
+	public void setProduct_productSeq1(String product_productSeq1) {
+		this.product_productSeq1 = product_productSeq1;
+	}
+	public String getMember_memberSeq() {
+		return member_memberSeq;
+	}
+	public void setMember_memberSeq(String member_memberSeq) {
+		this.member_memberSeq = member_memberSeq;
+	}
+	
+	
+	
+
+}
