0d06a273db1b96fb4b9684dc0571334fd0103ae1
countercrow <hongoppa@naver.com>
2023-08-10 17:33:24 +0900

    비교테이블에 필요한 서비스 제작

--- src/main/java/com/crowmarket/app/infra/common/product/ProductService.java
+++ src/main/java/com/crowmarket/app/infra/common/product/ProductService.java
@@ -10,6 +10,7 @@ public interface ProductService {
 	public List<Product> selectList(Model model);
 	public List<Product> shSelectList(ProductVo vo);
 	public Product selectOne(ProductVo vo);
+	public Product selectCompetition1(ProductVo vo);
 	public int update(Product dto); 
 	public int updele(Product dto); 
 	public int save(Product dto); 
