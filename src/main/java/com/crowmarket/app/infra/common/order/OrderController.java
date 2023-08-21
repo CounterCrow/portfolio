2b2e3d9c8e3cd365cf2223932a0fd3b5047d6cee
countercrow <hongoppa@naver.com>
2023-08-21 17:27:54 +0900

    필요 없는 임포트 제거 및 주문에 대한 ajax컨트롤러 구현

--- src/main/java/com/crowmarket/app/infra/common/order/OrderController.java
+++ src/main/java/com/crowmarket/app/infra/common/order/OrderController.java
@@ -1,9 +1,9 @@
 package com.crowmarket.app.infra.common.order;
 
+
+
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,20 +18,18 @@ public class OrderController {
 	@Autowired
 	private OrderServiceImpl service;
 	
-	@RequestMapping(value="userOrder")
-	public String userOrder(Order dto, HttpServletRequest request) {
-		service.userOrder(dto);
-		String referer = request.getHeader("Referer");
-	    return "redirect:"+ referer;
-	}
 	@ResponseBody
-	@RequestMapping(value="setModalVal")
-	public Map<String,Object> setModalVal(Order dto) throws Exception{
-	Map<String,Object> returnMap = new HashMap<String,Object>();
-	
-		returnMap.put("rt","success");
-		returnMap.put("rt","success");
-	return returnMap;
+	@RequestMapping(value="userOrder")
+	public  Map<String,Object> userOrder(Order dto)throws Exception {
+		Map<String,Object> returnMap = new HashMap<String,Object>();
+		Integer userOrder = service.userOrder(dto);
+		if(userOrder != null && userOrder != 0) {
+			returnMap.put("rt","success");
+		}else {
+			returnMap.put("rt","fail");
+		}
+		return returnMap;
 	}
+	
 
 }
