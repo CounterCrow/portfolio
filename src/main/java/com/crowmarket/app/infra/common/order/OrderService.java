63a223d3340056a2ae1cc2ad281574f1cf686d2e
countercrow <hongoppa@naver.com>
2023-08-22 17:44:13 +0900

    주문에 관련 서비스 생성

--- src/main/java/com/crowmarket/app/infra/common/order/OrderService.java
+++ src/main/java/com/crowmarket/app/infra/common/order/OrderService.java
@@ -1,7 +1,10 @@
 package com.crowmarket.app.infra.common.order;
 
+import java.util.List;
 
 public interface OrderService {
 
 	public int userOrder(Order dto);
+	public List<Order> selectUserOrderList(OrderVo vo);
+	public int orderCount(OrderVo vo);
 }
