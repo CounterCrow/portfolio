629d54d815278f515a6ddb3e26821ed965565152
countercrow <hongoppa@naver.com>
2023-08-22 17:44:29 +0900

    주문목록에 대한 서비스 임플리먼트 생성

--- src/main/java/com/crowmarket/app/infra/common/order/OrderServiceImpl.java
+++ src/main/java/com/crowmarket/app/infra/common/order/OrderServiceImpl.java
@@ -1,5 +1,7 @@
 package com.crowmarket.app.infra.common.order;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,18 @@ public class OrderServiceImpl implements OrderService{
 	public int userOrder(Order dto) {
 		
 		return dao.userOrder(dto);
+	}
+
+	@Override
+	public List<Order> selectUserOrderList(OrderVo vo) {
+		
+		return dao.selectUserOrderList(vo);
+	}
+
+	@Override
+	public int orderCount(OrderVo vo) {
+		System.out.println("서비스 : "+vo.getKeyMember_memberSeq());
+		return dao.orderCount(vo);
 	}
 	
 	
