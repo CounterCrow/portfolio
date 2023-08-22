45b8e9495d527e73f5eb752a6d817453c73a3d1f
countercrow <hongoppa@naver.com>
2023-08-22 17:43:37 +0900

    페이징관련 DAO+리스트 불러오는것에 대한 DAO

--- src/main/java/com/crowmarket/app/infra/common/order/OrderDao.java
+++ src/main/java/com/crowmarket/app/infra/common/order/OrderDao.java
@@ -1,10 +1,13 @@
 package com.crowmarket.app.infra.common.order;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public class OrderDao {
@@ -14,6 +17,8 @@ public class OrderDao {
 		private SqlSession  sqlSession;
 		private static String namespace = "com.crowmarket.app.infra.common.order.orderMapp";
 		
+		public List<Order> selectUserOrderList(OrderVo vo){return sqlSession.selectList(namespace+".selectUserOrderList",vo);}
 		public int userOrder(Order dto) {return sqlSession.insert(namespace+".userOrder",dto);}
+		public int orderCount(OrderVo vo) {return sqlSession.selectOne(namespace+".orderCount" ,vo);}
 	
 }
